6386b5e4eb731542587b8f9635ac9220ec0431d0
Emmie Ohnuki <emaohn@gmail.com>
2019-11-11 19:16:48 -0500

    Initial Commit

---
+++ TechLA Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechLA Weather App
+//
+//  Created by Emmie Ohnuki on 11/11/19.
+//  Copyright © 2019 Emmie Ohnuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
